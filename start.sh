8e3ccba7060673c0ae3ab88b4428f35d96527e15
Showmik Bose <officialshowmik@gmail.com>
2022-03-27 16:18:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ export PATH='/sbin:/bin:/usr/sbin:/usr/bin:~/.local/bin/'
 GIT_COMMIT_ID=`git rev-parse HEAD`
 FILE_NAME=`git diff-tree --no-commit-id --name-only -r \$GIT_COMMIT_ID`
 echo $FILE_NAME
-pcluster create -c \$FILE_NAME \$FILE_NAME  -r us-east-1
+pcluster create -c $FILE_NAME $FILE_NAME  -r us-east-1
