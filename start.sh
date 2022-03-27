874a09a227ac957dee0acb7026813213af92d976
Showmik Bose <officialshowmik@gmail.com>
2022-03-27 16:10:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 export PATH='/sbin:/bin:/usr/sbin:/usr/bin:~/.local/bin/'
 GIT_COMMIT_ID=`git rev-parse HEAD`
-echo "The value is \$GIT_COMMIT_ID"
 FILE_NAME=`git diff-tree --no-commit-id --name-only -r \$GIT_COMMIT_ID`
-echo "\$FILE_NAME"
+echo $FILE_NAME
 pcluster create -c \$FILE_NAME \$FILE_NAME  -r us-east-1
